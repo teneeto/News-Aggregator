a612e5d924b801d9c1488e1014acbab4647e0e98
Etotaziba Olei <teneeto@gmail.com>
2025-02-22 00:22:44 +0100

    add update newsService

--- src/services/newsService.ts
+++ src/services/newsService.ts
@@ -1,15 +1,21 @@
 import axios from "axios";
 
-// Define API endpoints
+// Define API endpoints Keys
 const API_KEYS = {
   newsApi: process.env.NEXT_PUBLIC_NEWS_API_KEY,
   guardian: process.env.NEXT_PUBLIC_GUARDIAN_API_KEY,
   nytimes: process.env.NEXT_PUBLIC_NYTIMES_API_KEY,
 };
 
+// Define API URIs
+const newsApiUri = "https://newsapi.org/v2/everything";
+const guardianApiUri = "https://content.guardianapis.com/search";
+const nytimesApiUri =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
 // Fetch news from NewsAPI
 export const fetchNewsAPI = async (query: string) => {
-  const response = await axios.get(`https://newsapi.org/v2/everything`, {
+  const response = await axios.get(newsApiUri, {
     params: { q: query, apiKey: API_KEYS.newsApi },
   });
   return response.data.articles;
@@ -17,7 +23,7 @@ export const fetchNewsAPI = async (query: string) => {
 
 // Fetch news from The Guardian
 export const fetchGuardianAPI = async (query: string) => {
-  const response = await axios.get(`https://content.guardianapis.com/search`, {
+  const response = await axios.get(guardianApiUri, {
     params: { q: query, "api-key": API_KEYS.guardian },
   });
   return response.data.response.results;
@@ -25,16 +31,13 @@ export const fetchGuardianAPI = async (query: string) => {
 
 // Fetch news from New York Times
 export const fetchNYTimesAPI = async (query: string) => {
-  const response = await axios.get(
-    `https://api.nytimes.com/svc/search/v2/articlesearch.json`,
-    {
-      params: { q: query, "api-key": API_KEYS.nytimes },
-    }
-  );
+  const response = await axios.get(nytimesApiUri, {
+    params: { q: query, "api-key": API_KEYS.nytimes },
+  });
   return response.data.response.docs;
 };
 
-// Fetch all news sources
+// Fetch all news sources together
 export const fetchAllNews = async (query: string) => {
   const [newsAPI, guardianAPI, nytimesAPI] = await Promise.all([
     fetchNewsAPI(query),
@@ -47,4 +50,25 @@ export const fetchAllNews = async (query: string) => {
     guardianAPI,
     nytimesAPI,
   };
+};
+
+// Fetch a single news article by its title
+export const fetchNewsDetail = async (newsId: string) => {
+  const decodedTitle = decodeURIComponent(newsId);
+
+  const newsArticles = await Promise.all([
+    fetchNewsAPI(decodedTitle),
+    fetchGuardianAPI(decodedTitle),
+    fetchNYTimesAPI(decodedTitle),
+  ]);
+
+  const allArticles = [
+    ...newsArticles[0],
+    ...newsArticles[1],
+    ...newsArticles[2],
+  ];
+
+  return allArticles.find((article) =>
+    article.title.toLowerCase().includes(decodedTitle.toLowerCase())
+  );
 };
